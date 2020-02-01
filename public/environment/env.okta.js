12cb994d82def5ab42c96d2a7edb72718e38bef5
Henrique Schmidt <hschmidt@avenuecode.com>
2020-02-01 16:13:32 -0300

    Fixes the authorization server URL

--- public/environment/env.okta.js
+++ public/environment/env.okta.js
@@ -1,4 +1,4 @@
 window.env = {
-  AUTHORIZATION_SERVER_URL: 'https://dev-361724.okta.com/',
+  AUTHORIZATION_SERVER_URL: 'https://dev-361724.okta.com/oauth2/default',
   CLIENT_ID: '0oa179pl9TPqA3n2d4x6'
 };
