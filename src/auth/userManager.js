74e814e584f0e0c2335ddaec3ca3531938a43c4d
Henrique Schmidt <hschmidt@avenuecode.com>
2020-02-01 12:09:31 -0300

    Changes the auth server to a hard coded value in order to test if it works on Netlifly

--- src/auth/userManager.js
+++ src/auth/userManager.js
@@ -1,12 +1,12 @@
 import { createUserManager } from 'redux-oidc';
 
 const userManagerConfig = {
-  client_id:  window.env.CLIENT_ID,
+  client_id:  '0oa179pl9TPqA3n2d4x6',
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
   post_logout_redirect_uri:  `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/home`,
   response_type: 'code',
   scope: 'openid',
-  authority: window.env.AUTHORIZATION_SERVER_URL,
+  authority: 'https://dev-361724.okta.com/',
   grantType: "authorization_code",
   silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
   automaticSilentRenew: true,
